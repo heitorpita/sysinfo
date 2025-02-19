b2ca39a088a6638fcc5b96b115a8bec686051f53
Heitor <heitor@tom.tom>
2025-02-19 15:55:33 -0300

    Your commit message

---
+++ sysinfo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "=== Informações do Sistema ==="
+echo "CPU Info:"
+lscpu | grep "Model name"
+
+echo -e "\nDiscos e Partições:"
+lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT
+
+echo -e "\nPlacas PCI:"
+lspci | grep -E "VGA|Network"
+
+echo -e "\nLogs recentes do sistema:"
+journalctl -n 10 --no-pager 
+
